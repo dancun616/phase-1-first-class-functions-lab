134ada2179090bdef1e506246d8c8edece8a501e
dancun616 <dancun616@gmail.com>
2022-11-28 12:54:50 +0300

    completed message

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function receivesAFunction(spy, another) {
+  return(spy() + "" + another);
+}
+function greetings() {
+  let name= 'Jacob';
+ return`Hello ${name}`;
+}
+console.log(greetings());
+function returnsANamedFunction() {
+ return greetings;
+}
+console.log(returnsANamedFunction());
+var fn= returnsANamedFunction();
+function returnsAnAnonymousFunction () {
+  return (function (){
+      return'It is nine in the morning'
+  })
+}
+console.log(returnsAnAnonymousFunction());
+var fn= returnsAnAnonymousFunction();
+white_check_mark
+eyes
+raised_hands
+
+
+
+
+
+
+
+
+
+
+
+
